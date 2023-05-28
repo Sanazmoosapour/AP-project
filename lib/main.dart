b2bf0742a699fa6c5c9578ea56b0c345270cd45d
Sanaz <sa.82mo@yahoo.com>
2023-05-28 12:13:21 +0430

    home page update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'dart:html';
+import 'dart:js';
 
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 
 void main() {
   runApp( MaterialApp(
@@ -8,6 +12,7 @@ void main() {
     home: Login(),
   ));
 }
+
 class Login extends StatelessWidget{
   TextEditingController nameController = TextEditingController();
   TextEditingController passController = TextEditingController();
@@ -41,7 +46,7 @@ class Login extends StatelessWidget{
               children: [
                 const Padding(
                     padding: EdgeInsets.only(bottom: 8.0,top: 180.0),
-                    child: Text("Log In",style: TextStyle(fontSize: 40),)
+                        child: Text("Log In",style: TextStyle(fontSize: 40),)
                 ),
                 Padding(
                   padding: const EdgeInsets.only(bottom: 8.0),
@@ -61,49 +66,51 @@ class Login extends StatelessWidget{
                         labelText: "pass",
                       )),
                 ),
-                Row(
-                  children: [
-                    Padding(
-                        padding: EdgeInsets.only(bottom: 8.0,right: 40.0),
-                        child: ElevatedButton(
-                            onPressed: (){
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) =>  home()),
-                              );
-                            },
-                            child: Text("log in")
-                        )
+                   Row(
+                     children: [
+                       Padding(
+                           padding: EdgeInsets.only(bottom: 8.0,right: 40.0),
+                           child: ElevatedButton(
+                               onPressed: (){
+                                 Navigator.push(
+                                   context,
+                                   MaterialPageRoute(builder: (context) =>  home()),
+                                 );
+                               },
+                               child: Text("log in")
+                           )
 
-                    ),
-                    Padding(
-                        padding: EdgeInsets.only(bottom: 8.0,right: 10.0),
-                        child: ElevatedButton(
-                            onPressed: (){
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) =>  signUp()),
-                              );
-                            },
-                            child: Text("sign up")
-                        )
+                       ),
+                       Padding(
+                           padding: EdgeInsets.only(bottom: 8.0,right: 10.0),
+                           child: ElevatedButton(
+                               onPressed: (){
+                                 Navigator.push(
+                                   context,
+                                   MaterialPageRoute(builder: (context) =>  signUp()),
+                                 );
+                               },
+                               child: Text("sign up")
+                           )
 
-                    )
-                  ],
-                )
+                       )
+                     ],
+                   )
 
               ],
             ),
           ),
         ),
       ),
-    );
+      );
     throw UnimplementedError();
   }
 
-
+  
 }
+
 class home extends StatelessWidget {
+    CarouselController controller = CarouselController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -118,7 +125,7 @@ class home extends StatelessWidget {
           actions: [
             PopupMenuButton(
               // add icon, by default "3 dot" icon
-                icon: const Icon(Icons.account_circle),
+               icon: const Icon(Icons.menu),
                 itemBuilder: (context){
                   return [
                     const PopupMenuItem<int>(
@@ -139,23 +146,32 @@ class home extends StatelessWidget {
                 },
                 onSelected:(value){
                   if(value == 0){
-                    print("My account menu is selected.");
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=> MyAccount()));
                   }else if(value == 1){
-                    print("Settings menu is selected.");
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=> settings()));
                   }else if(value == 2){
                     Navigator.push(context, MaterialPageRoute(builder: (context) =>  Login()));
                   }
                 }
             ),
-            ElevatedButton(
+            IconButton(
                 onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>  home()));
                 },
-                child: Container(
-                  color: Colors.teal,
-                  child: Icon(Icons.add_card),
-                )
+                icon: Icon(Icons.home),
+            ),
+            IconButton(
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) =>  shop()));
+              },
+              icon: Icon(Icons.shopping_cart),
+            ),
+            IconButton(
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context) =>  lib()));
+              },
+              icon: Icon(Icons.library_books_outlined),
             )
-
 
           ],
           title: const Center(
@@ -165,21 +181,74 @@ class home extends StatelessWidget {
 
           ),
         ),
-        body: Center(
-          child: ElevatedButton(
-              onPressed: (){
-                //Navigator.push(context, MaterialPageRoute(builder: (context) =>  home()));
-              },
-              child: Text("back")
-          ),
-        ),
-      ),
-    );
-    throw UnimplementedError();
+        body:  SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 200.0,width: 2000.0,child: CarouselSlider(
+                carouselController: controller ,
+
+                options: CarouselOptions(height: 400.0,autoPlay: true,aspectRatio: 2.0,
+                  enlargeCenterPage: true,),
+                items: [1,2,3,4,5].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      if(i==1) {
+                        return const Image(image: AssetImage('images/book1.jpg'));
+                      }
+                      if(i==2) {
+                        return const Image(image: AssetImage('images/book2.jpg'));
+                      }
+                      if(i==3) {
+                        return const Image(image: AssetImage('images/book3.jpeg'));
+                      }
+                      if(i==4) {
+                        return const Image(image: AssetImage('images/book4.jpg'));
+                      }
+                      if(i==5) {
+                        return const Image(image: AssetImage('images/book5.jpg'));
+                      }
+                      return const Image(image: AssetImage('images/book.png'));
+                    },
+                  );
+                }).toList(),
+              ),),
+              SizedBox( height: 500,
+                child: DefaultTabController(
+                  length: 3,
+                  child: Scaffold(
+                    appBar: AppBar(
+                      backgroundColor: Colors.teal,
+                      bottom:  const TabBar(
+                        tabs: [
+                          Tab(icon: Text("Ebooks")),
+                          Tab(icon: Text("Audiobooks")),
+                          Tab(icon: Text("Mylibrary")),
+                        ],
+                      ),
+                    ),
+                    body:  TabBarView(
+                      children: [
+                        Ebook(),
+                        Icon(Icons.directions_transit),
+                        Icon(Icons.directions_bike),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+          ),),
+
+
+
+      backgroundColor: Colors.tealAccent,
+    )
+
+    ); throw UnimplementedError();
+  }
   }
 
-
-}
 class signUp extends StatelessWidget {
   TextEditingController nameController = TextEditingController();
   TextEditingController passController = TextEditingController();
@@ -269,9 +338,9 @@ class signUp extends StatelessWidget {
                       )
                   ),
                 ],
-              )
-          ),
+          )
         ),
+      ),
       ),
     );
     throw UnimplementedError();
@@ -280,4 +349,280 @@ class signUp extends StatelessWidget {
 
 }
 
+class settings extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: myAppbar(),
+        body: const Center(
+          child: Text("settings"),
+      ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+}
+
+class Ebook extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView (
+          child: Column(
+            children: [
+              const SizedBox(height: 40.0,child: Text("Favorite",style: TextStyle(fontSize: 20),),),
+          SizedBox(
+          height: 300,
+          child: ListView(
+            // This next line does the trick.
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                // width: 160.0,
+                
+                child: InkWell(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                  },
+                  child: Image.network('https://newcdn.fidibo.com/images/books/85437_48595_normal.jpg?width=200'),
+                ),
+
+
+
+              ),
+              Container(
+                child: InkWell(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                  },
+                  child: Image.network('https://newcdn.fidibo.com/images/books/118215_13484_normal.jpg?width=200'),
+                ),
+              ),
+              Container(
+                child: InkWell(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                  },
+                  child: Image.network('https://newcdn.fidibo.com/images/books/90294_51440_normal.jpg?width=200'),
+                ),
+              ),
+              Container(
+                child:InkWell(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                  },
+                  child: Image.network('https://newcdn.fidibo.com/images/books/99473_44720_normal.jpg?width=200'),
+                ),
+              ),
+              Container(
+                child: InkWell(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                  },
+                  child: Image.network('https://newcdn.fidibo.com/images/books/100102_63516_normal.jpg?width=200'),
+                ),
+              ),
+            ],
+          ),
+        ),
+              const SizedBox(height: 40.0,child: Text("new releases",style: TextStyle(fontSize: 20),),),
+              SizedBox(
+                height: 300,
+                child: ListView(
+                  // This next line does the trick.
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      // width: 160.0,
+                      child:InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                        },
+                        child: Image.network('https://newcdn.fidibo.com/images/books/69380_61867_normal.jpg?width=200'),
+                      ),
+
+
+                    ),
+                    Container(
+                      child:InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                        },
+                        child: Image.network('https://newcdn.fidibo.com/images/books/69569_47206_normal.jpg?width=200'),
+                      ),
+                    ),
+                    Container(
+                      child:InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                        },
+                        child: Image.network('https://newcdn.fidibo.com/images/books/118203_76231_normal.jpg?width=200'),
+                      ),
+                    ),
+                    Container(
+                      child:InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                        },
+                        child: Image.network('https://newcdn.fidibo.com/images/books/109673_93017_normal.jpg?width=200'),
+                      ),
+                    ),
+                    Container(
+                      child: InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                        },
+                        child: Image.network('https://newcdn.fidibo.com/images/books/98322_90207_normal.jpg?width=200'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40.0,child: Text("top selling",style: TextStyle(fontSize: 20),),),
+              SizedBox(
+                height: 300,
+                child: ListView(
+                  // This next line does the trick.
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      // width: 160.0,
+                      child: InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                        },
+                        child: Image.network('https://newcdn.fidibo.com/images/books/69459_83225_normal.jpg?width=200'),
+                      ),
+
+                    ),
+                    Container(
+                      child:  InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                        },
+                        child: Image.network('https://newcdn.fidibo.com/images/books/70526_64499_normal.jpg?width=200'),
+                      ),
+                    ),
+                    Container(
+                      child:InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                        },
+                        child: Image.network('https://newcdn.fidibo.com/images/books/1998_41564_normal.jpg?width=200'),
+                      ),
+                    ),
+                    Container(
+                      child:InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                        },
+                        child: Image.network('https://newcdn.fidibo.com/images/books/62989_77673_normal.jpg?width=200'),
+                      ),
+                    ),
+                    Container(
+                      child:InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>detailview()));
+                        },
+                        child: Image.network('https://newcdn.fidibo.com/images/books/113320_76086_normal.jpg?width=200'),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+      ),
+        ),
+            backgroundColor: Colors.tealAccent,
+      )
+    );
+    throw UnimplementedError();
+  }
+
+}
+
+class lib extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: myAppbar(),
+        body: const Center(
+          child: Text("lib"),
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+}
+class detailview extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: myAppbar(),
+        body: const Center(
+          child: Text("mybook"),
+        ),
+        backgroundColor: Colors.tealAccent,
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+}
+
+class MyAccount extends StatelessWidget{
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    home: Scaffold(
+      appBar: myAppbar(),
+      body: const Center(
+        child: Text("Account"),
+      ),
+    ),
+  );
+  throw UnimplementedError();
+}
+
+}
+
+class shop extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: myAppbar(),
+        body: const Center(
+          child: Text("Wallet"),
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+}
+
+AppBar myAppbar(){
+  return AppBar(
+    backgroundColor:Colors.teal,
+    titleTextStyle: const TextStyle(
+      color: Colors.black,
+      fontSize: 50,
+      fontStyle: FontStyle.normal,
+    ),
+    title: const Center(
+      child: Text(
+        "fidiboo",
+      ),
+    ),
+
+  );
+
+}
 
